6b2908a18fd2e968161506d2d89cdc853dd3a956
0xKeynara <75882474+yusufwardana@users.noreply.github.com>
2025-10-08 17:00:18 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // Ubah baris ini dari './index.css' menjadi './globals.css'
-import './globals.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
